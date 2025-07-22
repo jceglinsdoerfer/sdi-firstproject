074f028e48e7886195787ee84364e5cee8264292
jceglinsdoerfer <jceglinsdoerfer@gmail.com>
2025-07-22 11:40:46 -0600

    added the readme with a short description

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sdi-firstproject
 project1
+This project reaches out to the crypto currency API (https://www.coinlore.com/cryptocurrency-data-api)
+We return the current top 10 crypto currencies and display them.  We then also prompt the user to input the number of currencies they have and we return the current value of those coins.  It also keeps the record if they user has multiple coins.
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-//the fetch
+//the fetch to get the top 10 crypto currencies
 function currentPrices (){
     return fetch('https://api.coinlore.net/api/ticker/?id=90,80,58,518,2710,48543,33285,2,46971,257')
         .then(response => {
@@ -16,13 +16,13 @@ function currentPrices (){
         .catch(error => console.log(error))    
 }
 
-
+//this loops through the fetch data and creates an object with the name and price
 currentPrices();
 function printAllNames(coins){
     const emptyCoinList = document.querySelector(".listofcoins")
     for (let i = 0; i < coins.length; i++) {
        var listItem = document.createElement("li") 
-       listItem.innerText =` ${coins[i].name}: $${coins[i].price_usd} `     
+       listItem.innerText =`${coins[i].name}  Price per coin $${coins[i].price_usd}`     
        emptyCoinList.appendChild(listItem)
     }
 }
@@ -32,29 +32,22 @@ function returnValue(coins) {
     let selection = document.getElementById("myDropdown");
     selection.addEventListener('change', (event) => {
         const selectedCoinID = event.target.value;
-        console.log(selectedCoinID);
         selectedPrice(selectedCoinID);
         })
         function selectedPrice (selectedCoinID) {
-            console.log('hello');
             return fetch(`https://api.coinlore.net/api/ticker/?id=${selectedCoinID}`)
         .then(response => {
-            console.log(selectedCoinID);
             if(!response.ok){
                 throw new Error('No information can be found!')
             }
             return response.json()
         })
         .then(selectedCoinsData => {
-            console.log(selectedCoinsData) 
             return selectedCoinsData;   
         })
         .then(coinValue => {
-            console.log(coinValue[0].price_usd);
             priceOfCoin = coinValue[0].price_usd
-            console.log(numberInputed, priceOfCoin)
             var totalValue = Math.ceil(numberInputed * priceOfCoin);
-            console.log(totalValue);
             const outputList = document.getElementById("output-list");
             var outputTotalValue = document.createElement("li")
             outputTotalValue.innerText = ` $${totalValue} `
@@ -67,7 +60,7 @@ function returnValue(coins) {
     const numberSelection = document.getElementById('numberInput');
         numberInput.addEventListener('input', (event) => {
         numberInputed = event.target.valueAsNumber;
-        console.log(numberInputed);    
+          
 
 
         
